8b37942bf29807807f52dce81b491452686af3f1
fulbrecht <13141050+fulbrecht@users.noreply.github.com>
2025-05-23 22:57:07 -0400

    updated index.html

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Serve the HTML file from the root directory
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'd3test.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // In-memory store for T-account data
